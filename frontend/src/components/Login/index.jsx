c6f9cbcd518e64858976942a01bd22f24896f6e5
Beatriz Leite <mbcleite@gmail.com>
2024-06-15 23:46:59 +0100

    frontend changes - missing phone optimization

--- frontend/src/components/Login/index.jsx
+++ frontend/src/components/Login/index.jsx
@@ -6,6 +6,9 @@ import address_icon from './../../img/icons/address.svg'
 import './index.css';
 import logo from './../../img/logo/geo_report_white_text.svg'
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+
 import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 
 
@@ -54,48 +57,50 @@ const Login = ({ setToken }) => {
     };
 
 
-
     return (
-        <Container fluid className="vh-100 d-flex align-items-center bg-black">
-            <Row className="w-100">
+        <Container fluid className="vh-1000 d-flex align-items-center bg-black">
+            <Row className="vh-1000">
                 <Col md={6} className="d-flex align-items-center justify-content-center">
                     <img src={logo} alt="Your Logo" className="img-fluid" style={{ maxWidth: '500px' }} /> {/* Add maxWidth style */}
                 </Col>
                 <Col md={6} className="d-flex align-items-center justify-content-center flex-column">
-                    <div className="container" style={{display: 'flex', justifyContent: 'center', maxWidth: '650px'}}>
+                    <div className="card login-signup-forgot" style={{display: 'flex', justifyContent: 'center', maxWidth: '650px', minWidth: '400px', 
+                        marginTop: '5vh', marginBottom: '10vh', marginLeft: '5vh', marginRight: '5vh'}}>
                         <div className="header">
                             <div className="text">{action}</div>
                             <div className="underline"></div>
                         </div>
-                        <div className="inputs">    
+                        <form>
+                            <div className="inputs">    
                             {action==="Login"?<div></div>:
                                 <><div className="input">
                                     <img className="svg" src={address_icon} alt="Last Name Icon" />
-                                    <input type="text" className="input-text" required />
+                                    <input type="text" className="form-control" required />
                                     <label className="placeholderx">First Name</label>
                                 </div>
                                 <div className="input">
                                     <img className="svg" src={address_icon} alt="Last Name Icon" />
-                                    <input type="text" className="input-text" required />
+                                    <input type="text" className="form-control" required />
                                     <label className="placeholderx">Last Name</label>
                                 </div>
                                 <div className="input">
                                     <img src={email_icon} alt="E-mail Icon" />
-                                    <input type="text" className="input-text" required />
+                                    <input type="text" className="form-control" required />
                                     <label className="placeholderx">E-Mail</label>
                                 </div></>}
                             <div className="input">
                                 <img src={user_icon} alt="User Icon"/>
-                                <input type="text" className="input-text" required />
+                                <input type="text" className="form-control" required />
                                 <label className="placeholderx">Username</label>
                             </div>
                             <div className="input">
                                 <img src={password_icon} alt="Password Icon" />
-                                <input type="password" className="input-text" required />
+                                <input type="password" className="form-control" required />
                                 <label className="placeholderx">Password</label>
 
                             </div>
                         </div>
+                        </form>
                         {action === "Sign Up" ? <div></div> :
                         <div className="forgot-password"> Forgot Password? <span>Click Here!</span></div>}
                         <div className="submit-container">
