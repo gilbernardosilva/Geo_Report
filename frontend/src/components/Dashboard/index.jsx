cf6950cbd1bae3c65b5b3b8c5f57a446208fee89
Nuno Moutinho <77934211+keisak1@users.noreply.github.com>
2024-06-19 23:21:57 +0100

    Dashboard changed, mobile implementation

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,72 +1,25 @@
-import React, { } from 'react';
+import React, { useState } from 'react';
 import './index.css';
-import logo from './../../img/logo/geo_report_no_text.png'
-import { Container, Row, Col } from 'react-bootstrap';
-import home from './../../img/icons/home.svg'
-import issues from './../../img/icons/issues.svg'
-import logout from './../../img/icons/logout.svg'
-import profile from './../../img/icons/profile.svg'
-import settings from './../../img/icons/settings.svg'
-import { Link } from 'react-router-dom';
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { Container, Col } from 'react-bootstrap';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import CustomNavbar from './../Navbar';
 import "leaflet/dist/leaflet.css";
-import useLogout from './../../hooks/useLogout.js'
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
+
+
 
 function Dashboard({ setToken }) {
 
-    const { handleLogout } = useLogout(setToken);
     const { t } = useTranslation();
-
     return (
-        <Container fluid className="dashboard-container">
-            <Row className="justify-content-center mt-4 dashboard-row">
-                <Col xs={12} md={3} className="d-flex justify-content-end" style={{ width: 300 }}>
-                    <Link to="/dashboard" className="sidebar-header text-end btn">
-                        <img src={logo} alt="Your Logo" className="img-fluid logo-icon" />
-                    </Link>
-                </Col>
-                <Col xs={12} md={4} className="d-flex justify-content-center">
-                    <h2>REPORT AN ISSUE!</h2>
-                </Col>
-                <Col xs={12} md={3} className="d-flex justify-content-end">
-                </Col>
-            </Row>
+        <>
+            <CustomNavbar setToken={setToken} t={t}></CustomNavbar>
+            <Container fluid>
 
-            <Row className="justify-content-center dashboard-row flex-grow-1" >
-
-                <Col xs={12} md={3} className="sidebar order-md-1 d-flex">
-                    <Link to="/dashboard" className="sidebar-header text-end btn">
-                        <img src={home} alt="" className="img-fluid logo-icon me-2" />
-                        <h2>{t('home')}</h2>
-                    </Link>
-                    <Link to="/dashboard" className="sidebar-header text-end btn">
-                        <img src={issues} alt="" className="img-fluid logo-icon me-2" />
-                        <h2>{t('myIssues')}</h2>
-                    </Link>
-
-                    <div className="mt-auto"></div>
-                    <Link to="/profile" className="sidebar-header text-end btn">
-                        <img src={profile} alt="" className="img-fluid logo-icon me-2" />
-                        <h2>{t('profile')}</h2>
-                    </Link>
-                    <Link to="/dashboard" className="sidebar-header text-end btn">
-                        <img src={settings} alt="" className="img-fluid logo-icon me-2" />
-                        <h2>{t('settings')}</h2>
-                    </Link>
-                    <Link to="/" className="sidebar-header text-end btn">
-                        <img src={logout} alt="" className="img-fluid logo-icon me-2" />
-                        <h2 onClick={handleLogout}>{t('logout')}</h2>
-                    </Link>
-                </Col>
-
-
-                <Col xs={12} md={4} className="map-area order-md-2 d-flex">
                         <MapContainer
                             center={[51.505, -0.09]}
                             zoom={13}
-                            scrollWheelZoom={false}
-                            style={{ height: "100%", width: "100%" }}>
+                            scrollWheelZoom={true}>
                             <TileLayer
                                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -77,12 +30,9 @@ function Dashboard({ setToken }) {
                                 </Popup>
                             </Marker>
                         </MapContainer>
-                </Col>
 
-                <Col xs={12} md={3} className="info-sidebar order-md-3">
-                </Col>
-            </Row>
-        </Container>
+            </Container>
+        </>
     );
 }
 
---
+++ frontend/src/components/Navbar/index.jsx
@@ -0,0 +1,81 @@
+import logo from './../../img/logo/geo_report_no_text.png'
+import { Container, Row, Col, Button, Form, Nav, Navbar, NavDropdown, Offcanvas } from 'react-bootstrap';
+import React, { useState } from 'react';
+import home from './../../img/icons/home.svg'
+import issues from './../../img/icons/issues.svg'
+import logout from './../../img/icons/logout.svg'
+import profile from './../../img/icons/profile.svg'
+import settings from './../../img/icons/settings.svg'
+import { Link } from 'react-router-dom';
+import useLogout from './../../hooks/useLogout';
+import LanguageSwitcher from '../LanguageSwitcher/index.jsx';
+
+function CustomNavbar({ setToken, t }) {
+
+    const { handleLogout } = useLogout(setToken);
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    const handleLanguageChange = (newAction) => {
+        console.log(newAction);
+    };
+    return (
+        <Navbar expand={false} bg="dark" variant="dark" className="mb-3">
+            <Container fluid>
+
+                <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} onClick={handleShow} />
+
+                <Navbar.Brand href="#">
+                    <Link to="/dashboard"   >
+                        <img src={logo} alt="Your Logo" className="img-fluid logo-icon" />
+                    </Link>
+                </Navbar.Brand>
+                <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-false`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-false`}
+                    show={show}
+                    onHide={handleClose}
+                >
+                    <Offcanvas.Header closeButton className="bg-dark">
+                        <Offcanvas.Title style={{ color: 'white', fontSize: '2rem' }} id={`offcanvasNavbarLabel-expand-lg`}>
+                            Menu
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body className="bg-dark d-flex flex-column">
+                        <div className="sidebar-content flex-column align-items-start flex-grow-1">
+                            <Link to="/dashboard" className="text-end btn d-flex align-items-center" style={{ textDecoration: 'none', color: 'white' }}>
+                                <img src={home} alt="" className="img-fluid logo-icon me-2" />
+                                <p style={{ fontSize: "1.5rem" }}>{t('home')}</p>
+                            </Link>
+                            <Link to="/dashboard" className="text-end btn d-flex align-items-center" style={{ textDecoration: 'none', color: 'white' }}>
+                                <img src={issues} alt="" className="img-fluid logo-icon me-2" />
+                                <p style={{ fontSize: "1.5rem" }}>{t('myIssues')}</p>
+                            </Link>
+                            <Link to="/profile" className="text-end btn d-flex align-items-center" style={{ textDecoration: 'none', color: 'white' }}>
+                                <img src={profile} alt="" className="img-fluid logo-icon me-2" />
+                                <p style={{ fontSize: "1.5rem" }}>{t("profile")}</p>
+                            </Link>
+                        </div>
+                        <div className="text-center text-white mb-3">
+                            <h3>{t('aboutGeo')}</h3>
+                            <small>{t('about')}</small>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between w-100">
+
+                            <Link to="/dashboard" className="sidebar-header">
+                                <img src={settings} alt="" className="img-fluid logo-icon me-2" />
+                            </Link>
+                            <LanguageSwitcher onLanguageChange={handleLanguageChange}></LanguageSwitcher>
+                            <Link to="/" className="sidebar-header">
+                                <img src={logout} alt="" className="img-fluid logo-icon me-2" onClick={handleLogout} />
+                            </Link>
+                        </div>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+
+            </Container>
+        </Navbar>
+    )
+};
+
+export default CustomNavbar
