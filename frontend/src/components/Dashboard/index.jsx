033be611d70974c663caaf510ebc85036a131847
Nuno Moutinho <77934211+keisak1@users.noreply.github.com>
2024-06-20 21:52:34 +0100

    Update index.jsx

--- frontend/src/components/Dashboard/index.jsx
+++ frontend/src/components/Dashboard/index.jsx
@@ -1,14 +1,45 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import { Container, Col } from 'react-bootstrap';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import L from "leaflet";
 import CustomNavbar from './../Navbar';
 import "leaflet/dist/leaflet.css";
 import { useTranslation } from "react-i18next";
-
+import icon from "leaflet/dist/images/marker-icon.png";
+import iconShadow from "leaflet/dist/images/marker-shadow.png";
 
 
 function Dashboard({ setToken }) {
+
+    const [userPosition, setUserPosition] = useState(null);
+
+    useEffect(() => {
+        function getLocation() {
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(showPosition, showError); // Use watchPosition to track changes
+            } else {
+                console.log("Geolocation is not supported by this browser.");
+            }
+        }
+
+        function showPosition(position) {
+            setUserPosition([position.coords.latitude, position.coords.longitude]);
+        }
+
+        function showError(error) {
+            console.error("Error getting geolocation:", error);
+        }
+
+        getLocation();
+    }, []);
+
+    const customIcon = new L.Icon({
+        iconUrl: icon,
+        shadowUrl: iconShadow,
+        iconSize: [25, 41],
+        iconAnchor: [12, 41],
+    });
 
     const { t } = useTranslation();
     return (
@@ -16,20 +47,22 @@ function Dashboard({ setToken }) {
             <CustomNavbar setToken={setToken} t={t}></CustomNavbar>
             <Container fluid>
 
-                        <MapContainer
-                            center={[51.505, -0.09]}
-                            zoom={13}
-                            scrollWheelZoom={true}>
-                            <TileLayer
-                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            />
-                            <Marker position={[51.505, -0.09]}>
-                                <Popup>
-                                    A pretty CSS3 popup. <br /> Easily customizable.
-                                </Popup>
-                            </Marker>
-                        </MapContainer>
+                <MapContainer
+                    center={userPosition || [41.1561925, -8.5171909]}
+                    zoom={13}
+                    scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    {userPosition && ( // Render the marker only if userPosition is available
+                        <Marker position={userPosition} icon={customIcon}>
+                            <Popup>
+                                You are here! (Lat: {userPosition[0]}, Lng: {userPosition[1]})
+                            </Popup>
+                        </Marker>
+                    )}
+                </MapContainer>
 
             </Container>
         </>
